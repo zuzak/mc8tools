4ec6f244f19aeaf11b7f1b58dadc52b90d738c64
Douglas Gardner <douglas@chippy.ch>
2014-04-10 08:12:25 +0100

    rm debug table

--- index.php
+++ index.php
@@ -1,8 +1,43 @@
+<?php
+require 'settings.php';
+require 'utilities.php';
+$curl = curl_init();
+$url = $baseUrl . "/initiate&" . http_build_query( array(
+  'format' => 'json',
+  'oauth_callback' => 'oob',
+  'oauth_consumer_key' => $consumerID,
+  'oauth_version' => '1.0',
+  'oauth_nonce' => md5(microtime() . mt_rand()),
+  'oauth_timestamp' => time(),
+  'oauth_signature_method' => 'HMAC-SHA1'
+));
+
+$sig = sign_request( 'GET', $url );
+$url .= '&oauth_signature=' . urlencode( $sig );
+
+curl_setopt( $curl, CURLOPT_URL, $url );
+curl_setopt( $curl, CURLOPT_RETURNTRANSFER, true);
+$data = curl_exec( $curl );
+curl_close( $curl );
+$data = json_decode($data);
+
+session_start();
+$_SESSION['tokenKey'] = $data->key;
+$_SESSION['tokenSecret'] = $data->secret;
+session_write_close();
+
+$authUrl = $baseUrl . '/authorize&';
+$authUrl .= http_build_query( array(
+  'oauth_token' => $data->key,
+  'oauth_consumer_key' => $consumerID
+));
+?>
 <!DOCTYPE html>
 <html>
 <head>
   <title>Topic Cat</title>
   <link href="vector.css" rel="stylesheet">
+  <meta charset="utf-8">
 </head>
 <body>
   <h1>Topic cat tool</h1>
@@ -25,9 +60,8 @@
   <p>
     Please authenticate with OAuth to begin.
   </p>
-  <button class="progress" type="submit" name="oauth" formaction="" formmethod="post">
-    Sign in with Wikinews
-  </button>
+
+  <a href="<?php echo $authUrl; ?>" class="progress button">Sign in with Wikinews</a>
 
   <p class="footer">
     Created by <a href="https://en.wikinews.org/wiki/User:Microchip08">Microchip08</a>
